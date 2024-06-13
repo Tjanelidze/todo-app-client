e5d0f8adca496e5d12ca88b627e4301eda7462e8
Tedo <tedojanelidze7@gmail.com>
2024-06-13 22:23:13 +0400

    implemented dark mode

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,12 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import { IAuthenticationContext } from "../context/AuthenticationContext";
 import { useAuthentication } from "../hooks/useAuthentication";
+import { SunIcon, MoonIcon } from "@heroicons/react/16/solid";
+import { useState } from "react";
 
 export default function Header() {
   const { isAuthenticated, logout } =
     useAuthentication() as IAuthenticationContext;
 
   const navigate = useNavigate();
+  const [dark, setDark] = useState(false);
+
+  const darkModeHandler = () => {
+    setDark(!dark);
+    document.body.classList.toggle("dark");
+  };
   return (
     <header className="container flex h-[20vh] items-center justify-between">
       <a href="/" className="text-xl font-bold dark:text-white">
@@ -14,7 +22,13 @@ export default function Header() {
       </a>
 
       <nav>
-        <ul>
+        <ul className="flex items-center gap-8">
+          <li>
+            <button onClick={darkModeHandler}>
+              {dark && <SunIcon className="size-7 text-yellow-500" />}
+              {!dark && <MoonIcon className="size-7 text-yellow-500" />}
+            </button>
+          </li>
           <li>
             {isAuthenticated ? (
               <button
